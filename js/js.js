18b18d0212f4263b8046aa0d6c157f5b2cce65d6
hhoahi <tthhoa.19it3@vku.udn.vn>
2023-06-21 11:54:49 +0700

    GoBank

---
+++ js/js.js
@@ -0,0 +1,35 @@
+$('.slide-news').owlCarousel({
+    loop:false,
+    margin:20,
+    nav:true,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+// $('.slide-client').owlCarousel({
+//     loop:false,
+//     margin:10,
+//     nav:true,
+//     dots: false,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:2
+//         },
+//         1000:{
+//             items:2
+//         }
+//     }
+// })
